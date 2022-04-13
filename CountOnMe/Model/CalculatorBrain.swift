5aa22ccc12a1cb875527be2cf879bd923679b586
BabyGeek <babygeek83bis@gmail.com>
2022-04-13 11:18:45 +0700

    First commit, made MVC repositories pattern

---
+++ CountOnMe/Model/CalculatorBrain.swift
@@ -0,0 +1,13 @@
+//
+//  CalculatorBrain.swift
+//  CountOnMe
+//
+//  Created by Paul Oggero on 13/04/2022.
+//  Copyright © 2022 Vincent Saluzzo. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorBrain {
+    
+}
